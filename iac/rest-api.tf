8ad04b10b7808b9fd1724b79d80daeb579d08e57
Andre Lopes <andrevitorlopes@gmail.com>
2024-04-18 00:20:42 +0200

    Add more modesl and mappings

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -16,25 +16,29 @@ resource "aws_api_gateway_integration" "integration" {
 
   request_templates = {
     "application/json" = jsonencode({
-      statusCode : "200"
+      statusCode : 200
     })
   }
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id     = aws_api_gateway_rest_api.api.id
-  resource_id     = aws_api_gateway_rest_api.api.root_resource_id
-  http_method     = aws_api_gateway_method.method.http_method
-  status_code     = "200"
-  response_models = {}
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
 }
 
 resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id        = aws_api_gateway_rest_api.api.id
-  resource_id        = aws_api_gateway_rest_api.api.root_resource_id
-  http_method        = aws_api_gateway_method.method.http_method
-  status_code        = aws_api_gateway_method_response.response_200.status_code
-  response_templates = {}
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+  response_templates = {
+    "application/json" = jsonencode({})
+  }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
