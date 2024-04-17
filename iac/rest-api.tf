7b0cb17cbdcb59b39a69b130695c9a5ff8fc7c2d
Andre Lopes <andrevitorlopes@gmail.com>
2024-04-18 00:06:39 +0200

    Add some stuff

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -13,20 +13,25 @@ resource "aws_api_gateway_integration" "integration" {
   resource_id = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
   type        = "MOCK"
+  request_templates = {
+    "statusCode" : "200"
+  }
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_rest_api.api.root_resource_id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = "200"
+  rest_api_id     = aws_api_gateway_rest_api.api.id
+  resource_id     = aws_api_gateway_rest_api.api.root_resource_id
+  http_method     = aws_api_gateway_method.method.http_method
+  status_code     = "200"
+  response_models = {}
 }
 
 resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_rest_api.api.root_resource_id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  rest_api_id        = aws_api_gateway_rest_api.api.id
+  resource_id        = aws_api_gateway_rest_api.api.root_resource_id
+  http_method        = aws_api_gateway_method.method.http_method
+  status_code        = aws_api_gateway_method_response.response_200.status_code
+  response_templates = {}
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
